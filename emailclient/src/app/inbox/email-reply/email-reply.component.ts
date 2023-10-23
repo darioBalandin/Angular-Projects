ac4153a261c9450c2a59624cd70f0a4247f2a1f3
darioBalandin <dariogarcia@capitole-consulting.com>
2023-10-23 08:47:25 +0200

    email reply

--- emailclient/src/app/inbox/email-reply/email-reply.component.ts
+++ emailclient/src/app/inbox/email-reply/email-reply.component.ts
@@ -1,22 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Email } from '../email.type';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-reply',
   templateUrl: './email-reply.component.html',
   styleUrls: ['./email-reply.component.css']
 })
-export class EmailReplyComponent {
+export class EmailReplyComponent implements OnChanges {
 
 
+  @Input()
   email: Email;
+
   showModal = false;
+
+  constructor(
+
+    private readonly emailService: EmailService
+  ) { }
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.email = {
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      subject: `RE: ${this.email.subject}`,
+      text: `\n\n\n-------- ${this.email.from} wrote:\n> ${this.email.text.replace(/\n/gi, '\n> ')}`
+    }
+  }
 
 
   onSubmit(email: Email) {
-
-
-
-
+    this.emailService.sendEmail(email).subscribe(() => {
+      this.showModal = false;
+    });
   }
 }
